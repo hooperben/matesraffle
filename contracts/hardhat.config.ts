f9ecb83a5d0f9e3e2e7d0cbc21e05a041ea861cf
Ben Hooper <ben@labrys.io>
2024-11-16 19:43:26 +0700

    chore: added veriy to hardhat

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,15 +1,46 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
 
 import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.27",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000000,
+          },
+          metadata: {
+            bytecodeHash: "none",
+          },
+        },
+      },
+    ],
+  },
   networks: {
     base: {
       url: `https://api.developer.coinbase.com/rpc/v1/base/${process.env.COINBASE_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY!],
     },
+  },
+  etherscan: {
+    apiKey: {
+      base: process.env.BASESCAN_API_KEY!,
+    },
+    customChains: [
+      {
+        network: "base",
+        chainId: 8453,
+        urls: {
+          apiURL: "https://api.basescan.org/api",
+          browserURL: "https://basescan.org",
+        },
+      },
+    ],
   },
 };
 
