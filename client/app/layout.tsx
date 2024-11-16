fb39169bd84369b6039c04a0f4b451558f87b35d
Ben Hooper <ben@labrys.io>
2024-11-17 04:41:33 +0700

    chore: added query client

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./globals.css";
 import { AppSidebar } from "@/components/app-sidebar";
@@ -10,8 +11,10 @@ import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "MatesRaffle",
-  description: "The most fair raffle software in the world.",
+  description: "The fairest raffle software in the world.",
 };
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -27,20 +30,22 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <DynamicContextProvider
-            settings={{
-              environmentId: "d6294e7a-dc6e-4379-af32-d4ccde0d212f",
-              walletConnectors: [EthereumWalletConnectors],
-            }}
-          >
-            <SidebarProvider>
-              <AppSidebar />
-              <main>
-                <SidebarTrigger />
-                {children}
-              </main>
-            </SidebarProvider>
-          </DynamicContextProvider>
+          <QueryClientProvider client={queryClient}>
+            <DynamicContextProvider
+              settings={{
+                environmentId: "d6294e7a-dc6e-4379-af32-d4ccde0d212f",
+                walletConnectors: [EthereumWalletConnectors],
+              }}
+            >
+              <SidebarProvider>
+                <AppSidebar />
+                <main>
+                  <SidebarTrigger />
+                  {children}
+                </main>
+              </SidebarProvider>
+            </DynamicContextProvider>
+          </QueryClientProvider>
         </ThemeProvider>
       </body>
     </html>
