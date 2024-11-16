c4581138dca6ff2568388b31fbd51e38a295a8f7
Ben Hooper <ben@labrys.io>
2024-11-16 18:00:39 +0700

    feat: added chainlink random call

--- contracts/contracts/MatesRaffle.sol
+++ contracts/contracts/MatesRaffle.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: GNU
 pragma solidity ^0.8.27;
 
-contract MatesRaffle {
-    constructor() {}
+import "./vrf/ChainLinkSubscriptionConsumer.sol";
+
+contract MatesRaffle is ChainLinkSubscriptionConsumer {
+    constructor() ChainLinkSubscriptionConsumer() {}
+
+    event MR_ChainLinkRequest(uint256 indexed _chainLinkRandomRequestId);
+
+    function requestRandomnessFromTheOracles() public {
+        uint256 chainLinkRandomRequestId = _requestRandomWords(true); // native payment
+
+        emit MR_ChainLinkRequest(chainLinkRandomRequestId);
+    }
 }
---
+++ contracts/contracts/vrf/ChainLinkSubscriptionConsumer.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+// An example of a consumer contract that relies on a subscription for funding.
+pragma solidity ^0.8.27;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+contract ChainLinkSubscriptionConsumer is VRFConsumerBaseV2Plus {
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
+
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256[] randomWords;
+    }
+
+    mapping(uint256 requestId => RequestStatus requestStatus) public s_requests;
+
+    // MatesRaffle Polygon Mainnet Sub Id
+    uint256 public s_subscriptionId =
+        26740131510072070510759061772352496168294574078875214684151409051291147389834;
+
+    // Past request IDs.
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    // The gas lane to use, which specifies the maximum gas price to bump to.
+    bytes32 public keyHash =
+        0x719ed7d7664abc3001c18aac8130a2265e1e70b7e036ae20f3ca8b92b3154d86; // 500 on polygon mainnet
+
+    uint32 public callbackGasLimit = 30_000;
+    uint16 public requestConfirmations = 3;
+    uint32 public numWords = 1;
+
+    /**
+     * HARDCODED FOR POLYGON MAINNET
+     * COORDINATOR: 0xec0Ed46f36576541C75739E915ADbCb3DE24bD77
+     */
+    constructor()
+        VRFConsumerBaseV2Plus(0xec0Ed46f36576541C75739E915ADbCb3DE24bD77)
+    {}
+
+    // Assumes the subscription is funded sufficiently.
+    // @param enableNativePayment: Set to `true` to enable payment in native tokens, or
+    // `false` to pay in LINK
+    function _requestRandomWords(
+        bool enableNativePayment
+    ) internal returns (uint256 requestId) {
+        // Will revert if subscription is not set and funded.
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({
+                        nativePayment: enableNativePayment
+                    })
+                )
+            })
+        );
+        s_requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        emit RequestSent(requestId, numWords);
+        return requestId;
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] calldata _randomWords
+    ) internal override {
+        require(s_requests[_requestId].exists, "request not found");
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        emit RequestFulfilled(_requestId, _randomWords);
+    }
+
+    function getRequestStatus(
+        uint256 _requestId
+    ) external view returns (bool fulfilled, uint256[] memory randomWords) {
+        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
+    }
+}
